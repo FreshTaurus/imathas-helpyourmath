9cb18894e5e81d2887ada65cd75a614ba178d49d
elevenstand7 <elevenstand7@gmail.com>
2024-05-07 16:34:09 -0400

    changingg header section

--- infoheader.php
+++ infoheader.php
@@ -1,12 +1,16 @@
-<div id="logo" style="display: flex; flex-direction: column;font-size:20px;">
-<span style="font-size:65px; color:#00cc00; margin-bottom: 20px;Arial, Helvetica, sans-serif;font-weight: bold;"><i><?php echo $installname; ?></i></span>
-<span> Welcome to our Free virtual  classroom</span>
+<div id="logo" style="display: flex; flex-direction: row;font-size:20px;">
+    <div style="display: flex; flex-direction: column;">
+        <span style="font-size:65px; color:#00cc00; margin-bottom: 20px;Arial, Helvetica, sans-serif;font-weight: bold;"><i><?php echo $installname; ?></i></span>
+        <span> Welcome to our Free virtual  classroom</span>
+    </div>
+    <div>
+        <ul >
+            <li><a href="<?php echo $imasroot; ?>/index.php">Home</a></li>
+            <li><a href="<?php echo $imasroot; ?>/diag/index.php">Diagnostics</a></li>
+        </ul>
+    </div>
 </div>
 
-<ul id="navlist">
-<li><a href="<?php echo $imasroot; ?>/index.php">Home</a></li>
-<li><a href="<?php echo $imasroot; ?>/diag/index.php">Diagnostics</a></li>
-</ul>
 
 <!--<div id="header">-->
 <!--<img class="floatright" src="--><?php //echo $imasroot; ?><!--/img/graph.gif" alt="graph image" />-->
